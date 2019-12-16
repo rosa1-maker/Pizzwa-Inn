319bfa97ba37d33dfa8128db42044411f2244a52
rosa1-maker <teresawanjiku2000@gmail.com>
2019-12-16 15:37:54 +0300

    created a function for from vlidation to ensure the user fills all the required information

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,19 @@ function Results (type,size,crust,toppings) {
 Results.prototype.order = function() {
     return "You have made an order of " + this.type + " pizza  with " + this.toppings + " as toppings and " + this.crust + " for crust ."
 };
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+       if (name === "") {
+      alert("please fill in your name");
+      return false;
+    } else if (email === "") {
+      alert("Please fill in your name and email");
+      return false;
+    } else {
+      alert(
+        name + ", We have received your message. Thank you for reaching out to us."
+      );
+    }
+    event.preventDefault();
+  }
