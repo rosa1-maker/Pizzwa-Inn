c9b5c833d6f7323d1e88f18f2d16ae608be1c4c4
rosa1-maker <teresawanjiku2000@gmail.com>
2019-12-17 10:47:15 +0300

    corrected the error of refreshing page when cheking out

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,15 @@
-function Results(type, size, crust, toppings) {
-  this.type = type;
-  this.size = size;
-  this.crust = crust;
-  this.toppings = toppings;
-}
-Results.prototype.order = function() {
-  return (
-    "You have made an order of " +
-    this.type +
-    " pizza  with " +
-    this.toppings +
-    " as toppings and " +
-    this.crust +
-    " for crust ."
-  );
-};
 function validateForm() {
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
-  if (name === "") {
-    alert("please fill in your name");
-    return false;
-  } else if (email === "") {
-    alert("Please fill in your name and email");
-    return false;
+  if (name$$email=== "") {
+    alert("please fill in your name and email");
+   
   } else {
     alert(name + ", Thank you for your comment.");
   }
   event.preventDefault();
 }
-var sizePrices = [1200, 800, 600];
+var sizePrices = [1000, 650, 350];
 var priceToppings = [100, 120, 80, 150, 200];
 var toppingsName = ["Pepperoni", "mushroom", "onion", "sausage", " bacon"];
 var crustNames = ["Crispy", "Stuffed", "Glutton-free"];
@@ -48,19 +28,22 @@ TotalPrice.prototype.finalTotal = function() {
   );
 };
 $(document).ready(function() {
-  $("form#order").submit(function(event) {
+  $("form#myorder").submit(function(event) {
     event.preventDefault();
-    var pizzaType = $("#type").val();
-    var pizzaSize = "#size".val();
-    var pizzaToppings = "#topings".val();
-    var pizzaQuant = parseInt($("#quantity").val());
-    var deliveryPizza = ("#delivery").val();
+    var pizzaType = $('#type').val();
+    var pizzaSize = parseInt($('#size').val());
+    var pizzaToppings=parseInt($('#topings').val());
+    var priceCrust =parseInt($('#crust').val());
+    var pizzaTop = $('#topings').val();
+    var pizzaQuant = parseInt($('#quantity').val());
+    var pizzaPick = parseInt($('#delivery').val());
     var price = sizePrices[pizzaSize - 1];
     var DeliveryCost = deliveryPrices[pizzaPick - 1];
-    var ToppingsCost = priceToppings[pizzaToppings - 1];
-    var crustCost = crustPrices[priceCrust - 1];
-    var topNames = toppingsName[pizzaTop - 1];
-    var crustName = crustNames[priceCrust - 1];
+    var ToppingsCost = priceToppings[pizzaToppings-1];
+    var crustCost = crustPrices[priceCrust-1]
+    var topNames = toppingsName[pizzaTop-1]
+    var crustName = crustNames[priceCrust-1] 
+
     newOrder = new Results(
       pizzaType,
       pizzaSize,
@@ -74,13 +57,55 @@ $(document).ready(function() {
       DeliveryCost,
       ToppingsCost,
       crustCost
-
     );
-    if (pizzaPick===1){
-        alert( newOrder.order());
+    function Results(type, size, crust, toppings) {
+        this.type = type;
+        this.size = size;
+        this.crust = crust;
+        this.toppings = toppings;
+      }
+      Results.prototype.order = function() {
+        return (
+          "You have made an order of " +
+          this.type +
+          " pizza  with " +
+          this.toppings +
+          " as toppings and " +
+          this.crust +
+          " for crust ."
+        );
+      };
+    if (pizzaPick === 1) {
+      alert(newOrder.order());
+      alert("Your bill is: " + newTotal.finalTotal());
+      alert("Thank you for shopping with us! ");
+    } else if (pizzaPick === 2) {
+      var location = prompt(
+        " Where would your like your Pizza to be Delivered? "
+      );
+      var locations = ["gpo", "ngong plaza", "trm", "cbd"];
+      if (
+        location !== locations[0] &&
+        location !== locations[1] &&
+        location !== locations[2] &&
+        location !== locations[3]
+      ) {
+        alert("Choose a location listed below");
+      } else {
+        prompt(
+          "Please enter your phone number to facilitate communication and faster delivery."
+        );
+        alert(
+          "Your order has been received and it will be delivered to " +
+            location +
+            " An additional 200/= will be charged for delivery."
+        );
+        alert(newOrder.order());
         alert("Your bill is: " + newTotal.finalTotal());
-        alert("Thank you for shopping with us! " )
+        alert(
+          "Thank you for shopping with us! Your Order will be arriving soon."
+        );
+      }
     }
-
   });
 });
